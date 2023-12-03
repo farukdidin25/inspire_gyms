68c8f33e2d8cd5f17da7c968ecdd37382a6ff1e6
Ömer Faruk Didin <fdidin59@gmail.com>
2023-12-03 18:12:08 +0300

    Drawe Fixed

--- lib/widgets/drawer_widget.dart
+++ lib/widgets/drawer_widget.dart
@@ -14,89 +14,87 @@ class MyDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: SingleChildScrollView(
-        child: Container(
-          color: AppColors.backgroundColor,
-          child: ListView(
-            children: [
-              DrawerHeader(
-                child: Center(
-                  child: GestureDetector(
-                    onTap: () {
-                      // Image'a tıklandığında HomePage'e git
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => HomePage(),
+      child: Container(
+        color: AppColors.backgroundColor,
+        child: ListView(
+          children: [
+            DrawerHeader(
+              child: Center(
+                child: GestureDetector(
+                  onTap: () {
+                    // Image'a tıklandığında HomePage'e git
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => HomePage(),
+                      ),
+                    );
+                  },
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Image.asset(
+                        'assets/icons/location.png',
+                        width: 70,
+                        height: 70,
+                        color: AppColors.iconColor,
+                      ),
+                      const SizedBox(height: 2),
+                      const GradientText(
+                        'Inspire Gym',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'EmonaW00-Condensed',
                         ),
-                      );
-                    },
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Image.asset(
-                          'assets/icons/location.png',
-                          width: 80,
-                          height: 80,
-                          color: AppColors.iconColor,
-                        ),
-                        const SizedBox(height: 2),
-                        const GradientText(
-                          'Inspire Gym',
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'EmonaW00-Condensed',
-                          ),
-                          gradient: LinearGradient(colors: [
-                            AppColors.secondaryColor,
-                            AppColors.iconColor,
-                          ]),
-                        ),
-                      ],
-                    ),
+                        gradient: LinearGradient(colors: [
+                          AppColors.secondaryColor,
+                          AppColors.iconColor,
+                        ]),
+                      ),
+                    ],
                   ),
                 ),
               ),
-              ListTile(
-                leading: const Icon(Icons.question_mark_sharp),
-                title: const Text(
-                  'Nasıl Kullanılır',
-                  style: TextStyle(fontSize: 20),
-                ),
-                onTap: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => const HowTo(),
-                    ),
-                  );
-                },
+            ),
+            ListTile(
+              leading: const Icon(Icons.question_mark_sharp),
+              title: const Text(
+                'Nasıl Kullanılır',
+                style: TextStyle(fontSize: 18),
               ),
-              ListTile(
-                leading: const Icon(Icons.info),
-                title: const Text(
-                  'Hakkımda',
-                  style: TextStyle(fontSize: 20),
-                ),
-                onTap: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => const InfoPage(),
-                    ),
-                  );
-                },
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const HowTo(),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.info),
+              title: const Text(
+                'Hakkımda',
+                style: TextStyle(fontSize: 18),
               ),
-              ListTile(
-                leading: const Icon(Icons.exit_to_app_rounded),
-                title: const Text(
-                  'Çıkış Yap',
-                  style: TextStyle(fontSize: 20),
-                ),
-                onTap: () {
-                  SystemNavigator.pop();
-                },
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const InfoPage(),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.exit_to_app_rounded),
+              title: const Text(
+                'Çıkış Yap',
+                style: TextStyle(fontSize: 18),
               ),
-            ],
-          ),
+              onTap: () {
+                SystemNavigator.pop();
+              },
+            ),
+          ],
         ),
       ),
     );
